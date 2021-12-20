3645ab525ade94bac4bf06c8759280ee7abe0bdc
FreshDev <billyhans90@gmail.com>
2021-12-20 14:36:00 +0100

    Fix the updat bug

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -5,7 +5,7 @@ if(isset($_GET['p_id'])) {
     $the_post_id = $_GET['p_id'];
 }
 
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
                             $select_posts_by_id = mysqli_query($connection, $query);
 
                             while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
@@ -19,6 +19,40 @@ $query = "SELECT * FROM posts";
                                 $post_tags = $row['post_tags'];
                                 $post_comment_count = $row['post_comment_count'];
                                 $post_date = $row['post_date'];
+                            }
+
+                            if(isset($_POST['update_post'])) {
+                                
+                                $post_author = $_POST['post_author'];
+                                $post_title = $_POST['post_title'];
+                                $post_category_id = $_POST['post_category'];
+                                $post_status = $_POST['post_status'];
+                                $post_image = $_FILES['image']['name'];
+                                $post_image_temp = $_FILES['image']['tmp_name'];
+                                $post_content = $_POST['post_content'];
+                                $post_tags = $_POST['post_tags'];
+
+                                move_uploaded_file($post_image_temp, "../images/$post_image");
+
+
+                                $query = "UPDATE posts SET ";
+                                $query .= "post_title = '{$post_title}', ";
+                                $query .= "post_category_id = '{$post_category_id}', ";
+                                $query .= "post_date = now(), ";
+                                $query .= "post_author = '{$post_author}', ";
+                                $query .= "post_status = '{$post_status}', "; 
+                                $query .= "post_tags = '{$post_tags}', "; 
+                                $query .= "post_content = '{$post_content}', "; 
+                                $query .= "post_image = '{$post_image}' ";
+                                $query .= "WHERE post_id = '{$the_post_id}' ";
+
+
+                                $update_post = mysqli_query($connection, $query);
+
+                                confirmQuery($update_post);
+                                
+
+
                             }
 
 
@@ -73,6 +107,9 @@ while ($row = mysqli_fetch_assoc($select_categories)) {
 
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image; ?>" alt="image">
+        <br>
+        <br>
+        <input type="file" name="image">
     </div>
 
     <div class="form-group">
@@ -86,6 +123,6 @@ while ($row = mysqli_fetch_assoc($select_categories)) {
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 </form>
