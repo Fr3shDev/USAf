7ef19f797a49dd314f92cf5e0f0331ec13e3f8f1
FreshDev <billyhans90@gmail.com>
2021-12-21 18:07:31 +0100

    Dashboard, adding widgets

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -26,12 +26,10 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-                        </li>
                         
                         
-                        <li class="divider"></li>
+                        
+                       
                         <li>
                             <a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                         </li>
@@ -78,9 +76,7 @@
                             </li>
                         </ul>
                     </li>  -->
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
-                    </li>
+                    
                 </ul>
             </div>
 
