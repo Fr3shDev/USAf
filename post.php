6aeafb4b1f9573a5585f93e19b2c4998875c3ce8
FreshDev <billyhans90@gmail.com>
2021-12-22 11:07:07 +0100

    Add views functionality to posts

--- post.php
+++ post.php
@@ -16,7 +16,14 @@
 
             if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
-            }
+
+                $view_query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id "; 
+                $send_query = mysqli_query($connection, $view_query);
+
+                if(!$send_query) {
+                    die("QUERY FAILED");
+                }
+            
 
             $query = "SELECT * FROM posts WHERE post_id=$the_post_id ";
             $select_all_posts_query = mysqli_query($connection, $query);
@@ -60,7 +67,14 @@
 
 
 
-            <?php } ?>
+            <?php }
+        
+        } else {
+            header("Location:index.php");
+        }
+        
+        
+        ?>
 
 
 
