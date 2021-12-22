3dc447f34b6ca5f2660a3347bc1409da6a7f898a
FreshDev <billyhans90@gmail.com>
2021-12-22 11:49:03 +0100

    Add pagination

--- index_old.php
+++ index_old.php
@@ -14,29 +14,9 @@
 
             <?php
 
-            $per_page = 5;
-
-            if(isset($_GET['page'])) {
-                $page = $_GET['page'];
-            } else {
-                $page = "";
-            }
-
-            if($page == "" || $page==1) {
-                $page_1 = 0;
-            } else {
-                $page_1 = ($page * $per_page) - $per_page;
-            }
-
-            $post_query_count = "SELECT * FROM posts";
-            $find_count = mysqli_query($connection, $post_query_count);
-            $count = mysqli_num_rows($find_count);
-
-            $count = ceil($count / $per_page);
 
 
-
-            $query = "SELECT * FROM posts ORDER by post_id DESC  LIMIT $page_1, $per_page ";
+            $query = "SELECT * FROM posts ORDER by post_id DESC";
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -65,7 +45,6 @@
                     </h1>
 
                     <!-- First Blog Post -->
-                    
                     <h2>
                         <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                     </h2>
@@ -119,24 +98,5 @@
     <!-- /.row -->
 
     <hr>
-
-    <ul class="pager">
-        <?php
-
-        for($i=1; $i<=$count; $i++) {
-
-            if($i == $page) {
-                echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-            } else {
-                echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-            }
-
-            
-        }
-
-        ?>
-
-
-    </ul>
 
     <?php include "includes/footer.php" ?>
