4e710cb94a46367aadcfd657851babf084c0ce8d
FreshDev <billyhans90@gmail.com>
2022-06-25 07:04:55 +0100

    Fixing admin frontend bugs

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -1,92 +1,94 @@
+<div class="table-responsive">
 <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Author</th>
-                                <th>Comment</th>
-                                <th>Email</th>
-                                <th>Status</th>
-                                <th>In Response to</th>
-                                <th>Date</th>
-                                <th>Approve</th>
-                                <th>UnApprove</th>
-                                
-                                <th>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Author</th>
+            <th>Comment</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>In Response to</th>
+            <th>Date</th>
+            <th>Approve</th>
+            <th>UnApprove</th>
 
-                            <?php
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
 
-                            $query = "SELECT * FROM comments";
-                            $select_comments = mysqli_query($connection, $query);
+        <?php
 
-                            while ($row = mysqli_fetch_assoc($select_comments)) {
-                                $comment_id = $row['comment_id'];
-                                $comment_post_id = $row['comment_post_id'];
-                                $comment_author = $row['comment_author'];
-                                $comment_content = $row['comment_content'];
-                                $comment_email = $row['comment_email'];                                
-                                $comment_status = $row['comment_status'];
-                                $comment_date = $row['comment_date'];
-                               
-                                echo "<tr>";
-                                echo "<td>{$comment_id}</td>";
-                                echo "<td>{$comment_author}</td>";
-                                echo "<td>{$comment_content}</td>";
-                                
+        $query = "SELECT * FROM comments";
+        $select_comments = mysqli_query($connection, $query);
 
+        while ($row = mysqli_fetch_assoc($select_comments)) {
+            $comment_id = $row['comment_id'];
+            $comment_post_id = $row['comment_post_id'];
+            $comment_author = $row['comment_author'];
+            $comment_content = $row['comment_content'];
+            $comment_email = $row['comment_email'];
+            $comment_status = $row['comment_status'];
+            $comment_date = $row['comment_date'];
 
-                                // $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
-                                //     $select_categories_id = mysqli_query($connection, $query);
-                                    
-                                //     while ($row = mysqli_fetch_assoc($select_categories_id)) {
-                                //         $cat_id = $row['cat_id'];
-                                //         $cat_title = $row['cat_title'];
+            echo "<tr>";
+            echo "<td>{$comment_id}</td>";
+            echo "<td>{$comment_author}</td>";
+            echo "<td>{$comment_content}</td>";
 
 
 
-                                // echo "<td>{$cat_title}</td>";
-                                //     }
+            // $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
+            //     $select_categories_id = mysqli_query($connection, $query);
+
+            //     while ($row = mysqli_fetch_assoc($select_categories_id)) {
+            //         $cat_id = $row['cat_id'];
+            //         $cat_title = $row['cat_title'];
+
+
+
+            // echo "<td>{$cat_title}</td>";
+            //     }
 
 
 
 
 
-                                echo "<td>{$comment_email}</td>";
-                                echo "<td>{$comment_status}</td>";
+            echo "<td>{$comment_email}</td>";
+            echo "<td>{$comment_status}</td>";
 
-                                    $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
-                                    $select_post_id_query = mysqli_query($connection, $query);
-                                    while($row = mysqli_fetch_assoc($select_post_id_query)) {
-                                        $post_id = $row['post_id'];
-                                        $post_title = $row['post_title'];
+            $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+            $select_post_id_query = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($select_post_id_query)) {
+                $post_id = $row['post_id'];
+                $post_title = $row['post_title'];
 
-                                        echo "<td><a href='../post.php?p_id=$post_id'>{$post_title}</a></td>";
-                                    }
-                                
-                                echo "<td>{$comment_date}</td>";
+                echo "<td><a href='../post.php?p_id=$post_id'>{$post_title}</a></td>";
+            }
 
-                                echo "<td><a href='comments.php?approved=$comment_id'>Approve</a></td>";
-                                echo "<td><a href='comments.php?unapproved=$comment_id'>Unapprove</a></td>";
-                                
-                                
-                                echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
-                                echo "</tr>";
-                            }
+            echo "<td>{$comment_date}</td>";
+
+            echo "<td><a href='comments.php?approved=$comment_id'>Approve</a></td>";
+            echo "<td><a href='comments.php?unapproved=$comment_id'>Unapprove</a></td>";
 
 
-                            ?>
+            echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
+            echo "</tr>";
+        }
 
-                            
 
-                        </tbody>
+        ?>
 
-                    </table>
 
-                    <?php
 
-if(isset($_GET['approved'])) {
+    </tbody>
+
+</table>
+    </div>
+
+<?php
+
+if (isset($_GET['approved'])) {
     $the_comment_id = $_GET['approved'];
 
     $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id=$the_comment_id ";
@@ -96,7 +98,7 @@ if(isset($_GET['approved'])) {
 }
 
 
-if(isset($_GET['unapproved'])) {
+if (isset($_GET['unapproved'])) {
     $the_comment_id = $_GET['unapproved'];
 
     $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id=$the_comment_id ";
@@ -108,19 +110,13 @@ if(isset($_GET['unapproved'])) {
 
 
 
-                    if(isset($_GET['delete'])) {
-                        $the_comment_id = $_GET['delete'];
+if (isset($_GET['delete'])) {
+    $the_comment_id = $_GET['delete'];
 
-                        $query = "DELETE FROM comments WHERE comment_id={$the_comment_id} ";
-                        $delete_query = mysqli_query($connection, $query);
+    $query = "DELETE FROM comments WHERE comment_id={$the_comment_id} ";
+    $delete_query = mysqli_query($connection, $query);
 
-                        header("Location:comments.php");
-                    }
+    header("Location:comments.php");
+}
 
-                    ?>
-
-
-
-
-
-                    
+?>
